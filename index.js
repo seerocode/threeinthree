31fefc3a53994c44bf36f69fff58f392fb73f474
Ro E <seerocode@users.noreply.github.com>
2018-04-10 11:56:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-function askGeolocation(){  
-    // check if browser support geolocation
-    if (navigator.geolocation) {
-        // ask permission and take positions    
-        navigator.geolocation.getCurrentPosition(setLatLong);
-    } else { 
-        alert("Geolocation is not supported by your browser.");
-    }
-};
+// function askGeolocation(){  
+//     // check if browser support geolocation
+//     if (navigator.geolocation) {
+//         // ask permission and take positions    
+//         navigator.geolocation.getCurrentPosition(setLatLong);
+//     } else { 
+//         alert("Geolocation is not supported by your browser.");
+//     }
+// };
 
-function setLatLong(position){  
-    alert('Thanks for accepting geolocation! :)');
+// function setLatLong(position){  
+//     alert('Thanks for accepting geolocation! :)');
 
-    // save position
-    _userLat = position.coords.latitude;
-    _userLng = position.coords.longitude;
+//     // save position
+//     _userLat = position.coords.latitude;
+//     _userLng = position.coords.longitude;
 
-    // we will check this function later
-    centerMap(_userLat, _userLng);    
-};
+//     // we will check this function later
+//     centerMap(_userLat, _userLng);    
+// };
+
+geocoder = new google.maps.Geocoder(); 
+
+var win= function(position){ 
+    userLat= position.coords.latitude; 
+    userLong= position.coords.longitude; 
+    var userLatLong = new google.maps.userLatLng(userLat, userLong); 
+    geocoder.geocode({'userLatLong': userLatLong}, function(results, status) { 
+        if (status == google.maps.GeocoderStatus.OK) { 
+            alert("The user's zipcode is "+results[0].address_components.postal_code); }}); }; 
+var fail= function(e){alert("GPS Failed");}; 
